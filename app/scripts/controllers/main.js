f6e4fdd82b4e255553c5fa1cc5812a216070eee3
Eddie Ng <wards.ng@gmail.com>
2014-05-27 11:41:02 -0400

    Infowindows working
    TODO: Add custom classes/options to infowindows, and other info

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -5,13 +5,13 @@ angular.module('waffellocatorApp')
   	function ($rootScope, $scope, yqlAPIservice, geocoder) {
 	    $scope.places = [];
 	    $scope.placeMarkers = []; // markers are in a random order
-	    $scope.infoWindow = null;
 
 	    $rootScope.$on('wdDataParsed', function(event, data) {
 	    	console.log("received event with data,", data);
 	    	$scope.places = data.places;
 
-	    	data.places.forEach(function(place, index) {
+	    	data.places.forEach(function(place, index, arr) {
+	    		// arr[index].showWindow = false;
 	    		var address = place.address;
 	    		var promise = geocoder.geocodeAddress(address);
 	   
@@ -20,18 +20,40 @@ angular.module('waffellocatorApp')
 	    				latitude: result.lat,
 	    				longitude: result.lng,
 	    				formattedAddress: result.formattedAddress,
-	    				id: index
+	    				id: index,
+	    				place: data.places[index],
+	    				showWindow: false,
+	    				// options passed to Google InfoWindow
+	    				markerOptions: {
+          			// boxClass: 'custom-info-window',
+          			// disableAutoPan: true
+          			opacity: 0.8
+        			},
+        			windowOptions: {
+        				pixelOffset: new google.maps.Size(0, 5),
+        				disableAutoPan: true,
+        				boxClass: 'custom-info-window'
+        			}
+	    			};
+
+	    			markerModel.click = function() {
+	    				console.log("OPENED", markerModel);
+	    				$scope.map.center.latitude = markerModel.latitude;
+							$scope.map.center.longitude = markerModel.longitude;
+							// $scope.map.zoom = 17;
+	    				markerModel.showWindow = true;
+	    				$scope.$apply();
+	    			};
+
+	    			markerModel.closeClick = function() {
+    					console.log("CLOSED");
+    					markerModel.showWindow = false;
+    					$scope.$apply();
 	    			};
 
 	    			//console.log('Success: ', address, result, index);
 	    			$scope.placeMarkers.push(markerModel);
 	    		}, function(error) {
-	    			var markerModel = {
-	    				latitude: '',
-	    				longitude: '',
-	    				formattedAddress: '',
-	    				id: index
-	    			};
 	    			//console.log('Failed: ', address, error, index);
 	    		});
 	    	});
@@ -47,19 +69,21 @@ angular.module('waffellocatorApp')
 			        latitude: 40.69847032728747,
 			        longitude: -73.9514422416687
 			    },
-			    zoom: 12
+			    zoom: 13
 			};
 
+			$scope.panPlace = function(index) {
 
-			$scope.panPlace = function(markerIndex) {
 				for (var i = 0; i < $scope.placeMarkers.length; i++) {
-					if ($scope.placeMarkers[i].id === markerIndex) {
-						$scope.infoWindow = $scope.placeMarkers[i];
-						break;
+					if ($scope.placeMarkers[i].id === index) {
+						//$scope.placeMarkers[i].click();
+						$scope.map.center.latitude = $scope.placeMarkers[i].latitude;
+						$scope.map.center.longitude = $scope.placeMarkers[i].longitude;
+						$scope.map.zoom = 17;
+						$scope.placeMarkers[i].showWindow = true;
 					}
 				}
 
-				console.log("Infowindow:", $scope.infoWindow);
 			};
 
   	}]);
