91de55ab82025b1378e45a21cda38a27a374600d
Eddie Ng <wards.ng@gmail.com>
2014-06-01 13:17:33 -0400

    Add web app

--- web.js
+++ web.js
@@ -1,6 +1,5 @@
-var gzippo = require('gzippo');
 var express = require('express');
+var port = process.env.PORT || 3000;
 var app = express();
-
-app.use(gzippo.staticGzip("" + __dirname + "/dist"));
-app.listen(process.env.PORT || 5000);
+app.use(express.static(__dirname + '/dist'));
+app.listen(port);
