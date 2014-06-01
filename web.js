e4fa6c83d84d6c3c8d2d10dd7ceacf6017081661
Eddie Ng <wards.ng@gmail.com>
2014-06-01 13:37:24 -0400

    updated package.json

--- web.js
+++ web.js
@@ -1,5 +1,7 @@
 var express = require('express');
+var path = require('path');
+
 var port = process.env.PORT || 3000;
 var app = express();
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(path.join(__dirname + '/dist')));
 app.listen(port);
